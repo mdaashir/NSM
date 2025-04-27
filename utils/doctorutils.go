02018c32170a9ff00a70468478a4342656311710
mdaashir <s.mohamedaashir@gmail.com>
2025-04-27 14:45:12 +0530

    feat: Improve error message for Nix installation with detailed instructions

--- utils/doctorutils.go
+++ utils/doctorutils.go
@@ -9,7 +9,16 @@ import (
 func CheckNixInstallation() error {
 	_, err := exec.LookPath("nix-env")
 	if err != nil {
-		return fmt.Errorf("nix-env command not found: %v", err)
+		return fmt.Errorf("nix-env command not found. To install Nix:\n\n" +
+			"Windows (via WSL2):\n" +
+			"1. Enable and setup WSL2\n" +
+			"2. Install Ubuntu or another Linux distro from Microsoft Store\n" +
+			"3. In WSL2, run: sh <(curl -L https://nixos.org/nix/install) --daemon\n\n" +
+			"Linux:\n" +
+			"Run: sh <(curl -L https://nixos.org/nix/install) --daemon\n\n" +
+			"macOS:\n" +
+			"Run: sh <(curl -L https://nixos.org/nix/install)\n\n" +
+			"For more information, visit: https://nixos.org/download.html")
 	}
 	return nil
 }
